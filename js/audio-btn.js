f60070bbfe27e6f72dcb9172895628af2802bde3
Misha Makarov <m.makarov90@bk.ru>
2023-11-15 21:08:36 +0300

    done

---
+++ js/audio-btn.js
@@ -0,0 +1,25 @@
+let audio
+let playbtn
+
+function initAudioPlayer() {
+	audio = new Audio()
+	audio.src = 'audio/Soular.mp3'
+	audio.loop = true
+	audio.play()
+	// Set object references
+	playbtn = document.getElementById('playpausebtn')
+	// Add Event Handling
+	playbtn.addEventListener('click', playPause)
+	// Functions
+	function playPause() {
+		if (audio.paused) {
+			audio.play()
+			playbtn.style.background = 'url(/img/audio-icons/pause.png) no-repeat'
+		} else {
+			audio.pause()
+			playbtn.style.background = 'url(/img/audio-icons/play.png) no-repeat'
+		}
+	}
+}
+
+window.addEventListener('load', initAudioPlayer);
