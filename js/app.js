38865def7b894bce2e8c70d21f925baef6a86568
Misha Makarov <m.makarov90@bk.ru>
2023-11-15 15:11:27 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	// autoplay: {
+	// 	delay: 1500,
+	// }
+});
+
+
+const swiperTwo = new Swiper('.sliderTwo', {
+
+	effect: 'cube',
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// autoplay: {
+	// 	delay: 3000,
+	// }
+});
