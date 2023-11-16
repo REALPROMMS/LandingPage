cf84a1d2aee425a6ee8b28bd5da0eb744e8c401f
Misha Makarov <m.makarov90@bk.ru>
2023-11-16 13:42:37 +0300

    done

--- js/observer.js
+++ js/observer.js
@@ -80,3 +80,61 @@ const observerBox3 = new IntersectionObserver(entries => {
 	});
 });
 observerBox3.observe(document.querySelector('.contact__box'));
+
+
+
+// !Текст трясется у новой секции
+const observerAbout = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const animText = entry.target.querySelector('.animText');
+		if (entry.isIntersecting) {
+			animText.classList.add('shakeAnim');
+			return; // если класс добавлен, продолжать уже не надо
+		}
+		// перемещение завершено, теперь надо удалить класс
+		animText.classList.remove('shakeAnim');
+	});
+});
+observerAbout.observe(document.querySelector('.about-title'));
+
+const observerHobby = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const animText = entry.target.querySelector('.animText');
+		if (entry.isIntersecting) {
+			animText.classList.add('shakeAnim');
+			return; // если класс добавлен, продолжать уже не надо
+		}
+		// перемещение завершено, теперь надо удалить класс
+		animText.classList.remove('shakeAnim');
+	});
+});
+observerHobby.observe(document.querySelector('.hobby-title'));
+
+const observerProject = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const animText = entry.target.querySelector('.animText');
+		if (entry.isIntersecting) {
+			animText.classList.add('shakeAnim');
+			return; // если класс добавлен, продолжать уже не надо
+		}
+		// перемещение завершено, теперь надо удалить класс
+		animText.classList.remove('shakeAnim');
+	});
+});
+observerProject.observe(document.querySelector('.project-title'));
+
+
+
+// !Текст появляется как глитч(матрица)
+const observerHeaderText = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const observerHeader = entry.target.querySelector('.observerHeader');
+		if (entry.isIntersecting) {
+			observerHeader.classList.add('headerTextAnim');
+			return; // если класс добавлен, продолжать уже не надо
+		}
+		// перемещение завершено, теперь надо удалить класс
+		observerHeader.classList.remove('headerTextAnim');
+	});
+});
+observerHeaderText.observe(document.querySelector('.header__info'));
