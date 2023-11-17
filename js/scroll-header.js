50ad89148ede549581847f07f2db950262c85cff
Misha Makarov <m.makarov90@bk.ru>
2023-11-17 18:11:44 +0300

    done

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -1,14 +1,37 @@
-window.onscroll = function () { myFunction() };
+// !Скрипт для плавного скролла по якорям на странице
 
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
 
-let header = document.getElementById("myHeader");
-
-let sticky = header.offsetTop;
-
-function myFunction() {
-	if (window.pageYOffset >= sticky) {
-		header.classList.add("sticky");
-	} else {
-		header.classList.remove("sticky");
-	}
+// Цикл по всем ссылкам
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault() // Предотвратить стандартное поведение ссылок
+		// Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+		const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+		// Плавная прокрутка до элемента с id = href у ссылки
+		document.querySelector(goto).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
 }
+
+
+
+
+// !Скрипт для липкой навигации
+// window.onscroll = function () { myFunction() };
+
+
+// let header = document.getElementById("myHeader");
+
+// let sticky = header.offsetTop;
+
+// function myFunction() {
+// 	if (window.pageYOffset >= sticky) {
+// 		header.classList.add("sticky");
+// 	} else {
+// 		header.classList.remove("sticky");
+// 	}
+// }
