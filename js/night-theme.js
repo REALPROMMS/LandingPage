77e9787127d34d6341492247374d41bffe7450dc
Misha Makarov <m.makarov90@bk.ru>
2023-11-17 11:46:42 +0300

    done

---
+++ js/night-theme.js
@@ -0,0 +1,6 @@
+// const nav = document.querySelector('.menu__toggle'),
+// 	toggleBtn = nav.querySelector('.toggle-btn');
+
+// toggleBtn.addEventListener('click', () => {
+// 	nav.classList.toggle('open')
+// });
