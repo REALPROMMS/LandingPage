a375b7255b6fea33ddd330a60d4896407cd25ff7
Misha Makarov <m.makarov90@bk.ru>
2023-11-18 09:56:52 +0300

    done

--- js/audio-btn.js
+++ js/audio-btn.js
@@ -1,23 +1,18 @@
-let audio
-let playbtn
-
 function initAudioPlayer() {
-	audio = new Audio()
-	audio.src = 'audio/Soular.mp3'
-	audio.loop = true
-	audio.pause()
+	const audio = new Audio('audio/Soular.mp3');
+	audio.loop = true;
 	// Set object references
-	playbtn = document.getElementById('playpausebtn')
+	const playbtn = document.getElementById('playpausebtn');
 	// Add Event Handling
-	playbtn.addEventListener('click', playPause)
+	playbtn.addEventListener('click', playPause);
 	// Functions
 	function playPause() {
 		if (audio.paused) {
-			audio.play()
-			playbtn.style.background = 'url(/img/audio-icons/pause2.png ) no-repeat'
+			audio.play();
+			playbtn.style.background = 'url(/img/audio-icons/pause.png ) no-repeat';
 		} else {
-			audio.pause()
-			playbtn.style.background = 'url(/img/audio-icons/play2.png) no-repeat'
+			audio.pause();
+			playbtn.style.background = 'url(/img/audio-icons/play.png) no-repeat';
 		}
 	}
 }
--- js/observer.js
+++ js/observer.js
@@ -1,245 +1,238 @@
+// !Текст трясется у новой секции (ABOUT HOBBY PROJECT) +++
+const observerTitle = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const animText = entry.target.querySelector('.animationTitle');
+		// if (entry.isIntersecting) // true or false {
+		// 	animText.classList.add('shakeAnim'); // добавялет новый класс
+		// 	return; // если класс добавлен, продолжать уже не надо
+		// }
+		// перемещение завершено, теперь надо удалить класс
+		// animText.classList.remove('shakeAnim');
+		animText.classList.toggle('shakeAnim', entry.isIntersecting); // написан блок в коментах
+	});
+});
+const elements = ['.about-title', '.hobby-title', '.project-title'];
+elements.forEach(element => observerTitle.observe(document.querySelector(element)));
+// observerTitle.observe(document.querySelector('.about-title'));
+// observerTitle.observe(document.querySelector('.hobby-title'));
+// observerTitle.observe(document.querySelector('.project-title'));
+
+// !----------------------------------
+
+// !Эффект падающей кнопки в header +++
+const observerBtn = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		const animText = entry.target.querySelector('.header__button-animation');
+		animText.classList.toggle('animationBtn', entry.isIntersecting);
+	});
+});
+observerBtn.observe(document.querySelector('.header__button'));
+
+// !----------------------------------
+
+// !Иконки выпадают сверх и пружинят, пока не остановятся +++
+// const observerIcons = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const animText = entry.target.querySelector('.social');
+// 		// if (entry.isIntersecting) {
+// 		// 	animText.classList.add('animationIcons');
+// 		// 	return; // если класс добавлен, продолжать уже не надо
+// 		// }
+// 		// перемещение завершено, теперь надо удалить класс
+// 		// animText.classList.remove('animationIcons');
+// 		animText.classList.toggle('animationIcons', entry.isIntersecting);
+// 	});
+
+// });
+// const elementsIcons = ['.social-box', '.social-box2', '.social-box3'];
+// elementsIcons.forEach(element => observerIcons.observe(document.querySelector(element)));
+// !---
+// if (entry.isIntersecting) {
+// 	animBox.classList.add('social-box-anim');
+// 	return; // если класс добавлен, продолжать уже не надо
+// }
+// // перемещение завершено, теперь надо удалить класс
+// animBox.classList.remove('social-box-anim');
+// !---
+// animBox.classList.toggle('social-box-anim', entry.isIntersecting);
+
+// !----------------------------------
+
 // !Иконки выпадают сверх и пружинят, пока не остановятся
 const observerIcons = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		const social = entry.target.querySelector('.social');
-		if (entry.isIntersecting) {
-			social.classList.add('anim-1');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		social.classList.remove('anim-1');
+		const animIcon = entry.target.querySelector('.social__box-icon');
+		animIcon.classList.toggle('animationIcons-1', entry.isIntersecting);
 	});
 });
-observerIcons.observe(document.querySelector('.social-box'));
+observerIcons.observe(document.querySelector('.social__box'));
 
 const observerIcons2 = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		const social = entry.target.querySelector('.social');
-		if (entry.isIntersecting) {
-			social.classList.add('anim-2');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		social.classList.remove('anim-2');
+		const animIcon = entry.target.querySelector('.social__box-icon');
+		animIcon.classList.toggle('animationIcons-2', entry.isIntersecting);
 	});
 });
-observerIcons2.observe(document.querySelector('.social-box2'));
+observerIcons2.observe(document.querySelector('.social__box2'));
 
 const observerIcons3 = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		const social = entry.target.querySelector('.social');
-		if (entry.isIntersecting) {
-			social.classList.add('anim-3');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		social.classList.remove('anim-3');
+		const animIcon = entry.target.querySelector('.social__box-icon');
+		animIcon.classList.toggle('animationIcons-3', entry.isIntersecting);
 	});
 });
-observerIcons3.observe(document.querySelector('.social-box3'));
+observerIcons3.observe(document.querySelector('.social__box3'));
 
-
+// !----------------------------------
 
 // !Прокруичвание в разные стороны иконок
 const observerBox = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
 		const animBox = entry.target.querySelector('.animBox');
-		if (entry.isIntersecting) {
-			animBox.classList.add('social-box-anim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animBox.classList.remove('social-box-anim');
+		animBox.classList.toggle('social__box-anim', entry.isIntersecting)
 	});
 });
-observerBox.observe(document.querySelector('.contact__box'));
 
-const observerBox2 = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const animBox = entry.target.querySelector('.animBox2');
-		if (entry.isIntersecting) {
-			animBox.classList.add('social-box-anim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animBox.classList.remove('social-box-anim');
-	});
-});
-observerBox2.observe(document.querySelector('.contact__box'));
+// observerBox.observe(document.querySelector('.contact__box'));
+
+const elementsIcon = ['.animationRotateIcons', '.animationRotateIcons2', '.animationRotateIcons3'];
+elementsIcon.forEach(elementsIcon => observerBox.observe(document.querySelector(elementsIcon)));
+
+// const observerBox2 = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const animBox = entry.target.querySelector('.animBox2');
+// 		animBox.classList.toggle('social__box-anim', entry.isIntersecting)
+// 	});
+// });
+// observerBox2.observe(document.querySelector('.contact__box'));
 
 
-const observerBox3 = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const animBox = entry.target.querySelector('.animBox3');
-		if (entry.isIntersecting) {
-			animBox.classList.add('social-box-anim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animBox.classList.remove('social-box-anim');
-	});
-});
-observerBox3.observe(document.querySelector('.contact__box'));
+// const observerBox3 = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const animBox = entry.target.querySelector('.animBox3');
+// 		animBox.classList.toggle('social__box-anim', entry.isIntersecting)
+// 	});
+// });
+// observerBox3.observe(document.querySelector('.contact__box'));
 
 
 
-// !Текст трясется у новой секции
-const observerAbout = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const animText = entry.target.querySelector('.animText');
-		if (entry.isIntersecting) {
-			animText.classList.add('shakeAnim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animText.classList.remove('shakeAnim');
-	});
-});
-observerAbout.observe(document.querySelector('.about-title'));
 
-const observerHobby = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const animText = entry.target.querySelector('.animText');
-		if (entry.isIntersecting) {
-			animText.classList.add('shakeAnim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animText.classList.remove('shakeAnim');
-	});
-});
-observerHobby.observe(document.querySelector('.hobby-title'));
-
-const observerProject = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const animText = entry.target.querySelector('.animText');
-		if (entry.isIntersecting) {
-			animText.classList.add('shakeAnim');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		animText.classList.remove('shakeAnim');
-	});
-});
-observerProject.observe(document.querySelector('.project-title'));
-
-
-
-// !Текст появляется как глитч(матрица)
+// !Заголовок появляется c Glitch эффектом в header +++
 const observerHeaderText = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		const observerHeader = entry.target.querySelector('.observerHeader');
+		const observerHeader = entry.target.querySelector('.observer-title-header');
 		if (entry.isIntersecting) {
-			observerHeader.classList.add('headerTextAnim');
+			observerHeader.classList.add('animationGlitchTitle');
 			return; // если класс добавлен, продолжать уже не надо
 		}
 		// перемещение завершено, теперь надо удалить класс
-		observerHeader.classList.remove('headerTextAnim');
+		observerHeader.classList.remove('animationGlitchTitle');
 	});
 });
 observerHeaderText.observe(document.querySelector('.header__info'));
 
 
+// !Эффект выезжающих панелей skills HTML
 
-// !Эффект падающей кнопки
-const observerBtn = new IntersectionObserver(entries => {
+const observerSkill = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
-		const observerHeader = entry.target.querySelector('.btn-flip');
+		const animText = entry.target.querySelector('.progress-line');
 		if (entry.isIntersecting) {
-			observerHeader.classList.add('animationBtn');
+			animText.classList.add('animationPanel');
 			return; // если класс добавлен, продолжать уже не надо
 		}
 		// перемещение завершено, теперь надо удалить класс
-		observerHeader.classList.remove('animationBtn');
+		animText.classList.remove('animationPanel');
 	});
 });
-observerBtn.observe(document.querySelector('.header__button'));
+const elementsSkill = ['.html', '.css', '.JavaScript', '.react', '.bootstrap', '.wordpress'];
+elementsSkill.forEach(element => observerSkill.observe(document.querySelector(element)));
 
 
 
-// !Эффект выезжающих панелей skills
-const observerHtml = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.html');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerHtml.observe(document.querySelector('.panels'));
+// const observerHtml = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.html');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerHtml.observe(document.querySelector('.panels'));
 
-// !CSS
-const observerCss = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.css');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerCss.observe(document.querySelector('.panels'));
+// // !CSS
+// const observerCss = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.css');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerCss.observe(document.querySelector('.panels'));
 
-// !JS
-const observerJs = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.JavaScript');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerJs.observe(document.querySelector('.panels'));
+// // !JS
+// const observerJs = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.JavaScript');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerJs.observe(document.querySelector('.panels'));
 
-// !REACT
-const observerReact = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.react');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerReact.observe(document.querySelector('.panels'));
+// // !REACT
+// const observerReact = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.react');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerReact.observe(document.querySelector('.panels'));
 
-// !BS
-const observerBs = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.bootstrap');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerBs.observe(document.querySelector('.panels'));
+// // !BS
+// const observerBs = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.bootstrap');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerBs.observe(document.querySelector('.panels'));
 
-// !WP
-const observerWp = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		const observerExp = entry.target.querySelector('.wordpress');
-		if (entry.isIntersecting) {
-			observerExp.classList.add('animationPanel');
-			return; // если класс добавлен, продолжать уже не надо
-		}
-		// перемещение завершено, теперь надо удалить класс
-		observerExp.classList.remove('animationPanel');
-	});
-});
-observerWp.observe(document.querySelector('.panels'));
+// // !WP
+// const observerWp = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		const observerExp = entry.target.querySelector('.wordpress');
+// 		if (entry.isIntersecting) {
+// 			observerExp.classList.add('animationPanel');
+// 			return; // если класс добавлен, продолжать уже не надо
+// 		}
+// 		// перемещение завершено, теперь надо удалить класс
+// 		observerExp.classList.remove('animationPanel');
+// 	});
+// });
+// observerWp.observe(document.querySelector('.panels'));
 
 
 
