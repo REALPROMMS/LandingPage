3c6857cf0385350821c28c40c71b6b6c252d1c21
Misha Makarov <m.makarov90@bk.ru>
2023-11-19 11:58:00 +0300

    done

---
+++ js/burger-nav.js
@@ -0,0 +1,33 @@
+// const icon = document.getElementById('toggler');
+// const nav = document.getElementById('navbar');
+// // functions
+// nav.style.display = 'none';
+// const showNav = (e) => {
+// 	e.target.parentElement.classList.toggle('change-backGround');
+// 	e.target.classList.toggle('clicked');
+// 	//setTimeout ==> because when i display:none for elements not animated so i delay it to be smooth
+// 	e.target.classList.contains('clicked') ? (nav.style.cssText = 'dispaly: flex;', setTimeout(() => nav.style.transform = 'translateY(0)', 300)) : (nav.style.transform = 'translateY(-100%)', setTimeout(() => nav.style.display = 'none', 700));
+// }
+// //events
+// icon.addEventListener('click', showNav);
+
+
+// !вешает анимацию на кнопку при клике и удаляет ее
+
+// let animateButton = function (e) {
+
+// 	e.preventDefault;
+// 	//reset animation
+// 	e.target.classList.remove('animate');
+
+// 	e.target.classList.add('animate');
+// 	setTimeout(function () {
+// 		e.target.classList.remove('animate');
+// 	}, 700);
+// };
+
+// let bubblyButtons = document.getElementsByClassName("bubbly-button2");
+
+// for (let i = 0; i < bubblyButtons.length; i++) {
+// 	bubblyButtons[i].addEventListener('click', animateButton, false);
+// }
--- js/scroll-header.js
+++ js/scroll-header.js
@@ -35,3 +35,7 @@ for (let anchor of anchors) {
 // 		header.classList.remove("sticky");
 // 	}
 // }
+
+
+
+
