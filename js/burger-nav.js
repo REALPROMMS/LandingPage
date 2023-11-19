675642bdb814de9fa564d1d4b4af8ec9e715c2ff
Misha Makarov <m.makarov90@bk.ru>
2023-11-20 00:34:01 +0300

    done

--- js/burger-nav.js
+++ js/burger-nav.js
@@ -1,15 +1,18 @@
-const icon = document.getElementById('toggler');
-const navigation = document.getElementById('navbar');
-// functions
-navigation.style.display = 'none';
-const showNav = (e) => {
-	e.target.parentElement.classList.toggle('change-backGround');
-	e.target.classList.toggle('clicked');
-	//setTimeout ==> because when i display:none for elements not animated so i delay it to be smooth
-	e.target.classList.contains('clicked') ? (navigation.style.cssText = 'dispaly: flex;', setTimeout(() => navigation.style.transform = 'translateY(0)', 300)) : (navigation.style.transform = 'translateY(-100%)', setTimeout(() => navigation.style.display = 'none', 700));
-}
-//events
-icon.addEventListener('click', showNav);
+// const icon = document.getElementById('toggler');
+// const navigation = document.getElementById('navbar');
+// // functions
+// navigation.style.display = 'none';
+// const showNav = (e) => {
+// 	e.target.parentElement.classList.toggle('change-backGround');
+// 	e.target.classList.toggle('clicked');
+// 	//setTimeout ==> because when i display:none for elements not animated so i delay it to be smooth
+// 	e.target.classList.contains('clicked') ? (navigation.style.cssText = 'dispaly: flex;', setTimeout(() => navigation.style.transform = 'translateY(0)', 300)) : (navigation.style.transform = 'translateY(-100%)', setTimeout(() => navigation.style.display = 'none', 700));
+// }
+// //events
+// icon.addEventListener('click', showNav);
+
+
+
 
 
 
--- js/full-screen.js
+++ js/full-screen.js
@@ -1,57 +1,10 @@
 // !ДОБАВЛЯЕТ НОВЫЙ КЛАСС И УДАЛЯЕМ ДИСПЛЕЙ NONE
-let screen = document.querySelector('.full__screen'),
-	// кнопка
-	full__screen = screen.querySelector('.full__screen-btn'),
 
-	all = document.querySelector('body'),
-	// главный селектор сайта
-	selectorAll = all.querySelector('.full-site');
+let screenBox = document.querySelector('.full__screen');
+let full__screen = document.querySelector('.full__screen-btn');
+let selectorAll = document.querySelector('.full-site');
 
-full__screen.addEventListener('click', () => {
-	if (selectorAll.classList.toggle('displayNoneAll') === true) {
-		selectorAll.classList.toggle('displayBlockAll') && selectorAll.classList.romove('displayNoneAll')
-	}
+const add = full__screen.addEventListener('click', () => {
+	selectorAll.classList.remove('none')
+	screenBox.classList.add('none')
 });
-
-// !---
-
-
-const observerScreen = new IntersectionObserver(items => {
-	items.forEach(item => {
-
-		const animSite = item.target.querySelector('.full-site');
-		if (item.isIntersecting) {
-			animSite.classList.add('displayNoneAll');
-		} else {
-			animSite.classList.remove('displayBlockAll');
-		}
-		let screen = document.querySelector('.full__screen');
-		let full__screen = screen.querySelector('.full__screen-btn');
-		full__screen.addEventListener('click', () => {
-			screen.classList.add('displayNoneAll')
-			return
-		});
-	});
-});
-observerScreen.observe(document.querySelector('body'));
-
-
-
-// const observerScreen = new IntersectionObserver(items => {
-// 	items.forEach(item => {
-
-// 		const animText = item.target.querySelector('.all');
-// 		if (item.isIntersecting) {
-// 			animText.classList.add('displayNoneAll');
-// 		} else {
-// 			animText.classList.remove('displayBlockAll');
-// 		}
-// 		let screen = document.querySelector('.full__screen');
-// 		let full__screen = screen.querySelector('.full__screen-btn');
-// 		full__screen.addEventListener('click', () => {
-// 			screen.classList.add('displayNoneAll')
-// 			return
-// 		});
-// 	});
-// });
-// observerScreen.observe(document.querySelector('body'));
