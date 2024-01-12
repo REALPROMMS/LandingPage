b4baf3a30c7a07b0c1acede41b3ba5b706e4d37c
Mihail Makarov <m.makarov90@bk.ru>
2024-01-12 14:04:19 +0300

    done

--- js/audio-btn.js
+++ js/audio-btn.js
@@ -1,22 +1,21 @@
 // !Настройки AUDIO, две кнопки play and pause на сайте появляются
 function initAudioPlayer() {
-	const audio = new Audio('audio/RG-Rose-Wasted.mp3');
-	audio.loop = true;
+	const audio = new Audio('audio/RG-Rose-Wasted.mp3')
+	audio.loop = true
 	// Set object references
-	const playbtn = document.getElementById('playpausebtn');
+	const playbtn = document.getElementById('playpausebtn')
 	// Add Event Handling
-	playbtn.addEventListener('click', playPause);
+	playbtn.addEventListener('click', playPause)
 	// Functions
 	function playPause() {
 		if (audio.paused) {
-			audio.play();
-			playbtn.style.background = 'url(/img/audio-icons/pause.png ) no-repeat';
+			audio.play()
+			playbtn.style.background = 'url(./img/audio-icons/pause.png) no-repeat'
 		} else {
-			audio.pause();
-			playbtn.style.background = 'url(/img/audio-icons/play.png) no-repeat';
+			audio.pause()
+			playbtn.style.background = 'url(./img/audio-icons/play.png) no-repeat'
 		}
 	}
 }
 
-window.addEventListener('load', initAudioPlayer);
-
+window.addEventListener('load', initAudioPlayer)
